004f574e8a0bfd09428abefbd675bbd8d026f227
TopiKo <topiko1987@gmail.com>
2020-01-27 19:13:50 +0100

    Talking too!

---
+++ Raspberry/ser_com3.py
@@ -0,0 +1,44 @@
+
+import time
+import serial
+import struct
+
+ser = serial.Serial(
+        port='/dev/serial0',
+        baudrate = 115200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=.00001
+)
+
+def talk(a,b,c):
+
+  for s in [1, a, b]:
+    send = struct.pack('>B', s)
+    ser.write(send)
+
+  s = ser.read(2)
+  return int.from_bytes(s, byteorder='big')
+  #return struct.unpack('>B', s)
+
+def act(i):
+  a = i >> 8
+  b = i & 0xff
+  c = 3
+  return a, b, c
+
+i = 0
+orient = 10
+t0 = time.time()
+while orient not in [0, 1023]:
+
+  a,b,c  = act(i)
+  orient = talk(a,b,c)
+
+
+  i+=1
+  if i%100 == 0:
+    t1 = time.time()
+    print('Freq: ', i/(t1-t0))
+
