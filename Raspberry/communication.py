d178e9575d703f15771699a42d2642343eacdbbd
TopiKo <topiko1987@gmail.com>
2020-02-02 14:43:01 +0100

    Decent prediction

--- Raspberry/communication.py
+++ Raspberry/communication.py
@@ -36,12 +36,14 @@ def talk(ser, cmd):
 def listen(ser):
   
   t0 = time.time()
-  while ser.in_waiting<1: pass
+  while ser.in_waiting<2: pass
   t1 = time.time()
 
   s = ser.read(2)
- 
-  orient = int.from_bytes(s, byteorder='big', signed = True)
+  time_pitch = ser.read(4)
 
-  return orient, t1 - t0
+  orient = int.from_bytes(s, byteorder='big', signed = True)
+  time_pitch = int.from_bytes(time_pitch, byteorder='big', signed = False)
+  #print('Imu time: ', time_pitch)
+  return orient, time_pitch, t1 - t0
 
--- Raspberry/talk_w_due_devel.py
+++ Raspberry/talk_w_due_devel.py
@@ -15,30 +15,33 @@ ser = serial.Serial(
 
 angle_factor = 20000.
 pi = 3.14159267
-report = False #True 
+report = True #False #True 
 # store time and pitch of last 3 time steps
-orient_arr = np.zeros((3, 5))  
+orient_arr = np.zeros((3, 6))  
 
 def act(orient_arr):
     
-  time.sleep(.05)
-  a = int(-(orient_arr[0,0]/90)*1024)
+  time.sleep(.015)
+  a = int(-(orient_arr[0,1]/90)*1024) if not np.isnan(orient_arr[0,1]) else 0
   return [0,a,a]
 
 def update_orient(orient_arr, orient, time_orient):
 
   orient = orient/angle_factor/pi*180
-  orient_arr[0,:2] = time_orient, orient 
+  orient_arr[0,:2] = time_orient/1e6, orient 
 
   return orient_arr
 
-def predict_orient(orient_arr, time_orient):
+def predict_orient(orient_arr, add_time):
   
   # fit 2 order poly to 3 values in orient arr 
   # --> w, dw/dt, d**2w/dt**2 
   # use these to predict values at time_orient
   p1, p2, p3 = orient_arr[:, 1] 
-  t1, t2, t3 = orient_arr[:, 0]
+  t1, t2, t3 = orient_arr[:, 0] - orient_arr[0,0]
+
+  if (np.diff(orient_arr[:, 0]) == 0).any(): 
+      print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
 
   a = (p1*(t2 - t3) - p2*(t1 - t3) + p3*(t1 - t2)) \
        /(t1**2*t2 - t1**2*t3 - t1*t2**2 + t1*t3**2 + t2**2*t3 - t2*t3**2)
@@ -47,17 +50,17 @@ def predict_orient(orient_arr, time_orient):
   c = (p1*t2*t3*(t2 - t3) - p2*t1*t3*(t1 - t3) + p3*t1*t2*(t1 - t2)) \
           /(t1**2*t2 - t1**2*t3 - t1*t2**2 + t1*t3**2 + t2**2*t3 - t2*t3**2)
   
+  time_orient = add_time #np.diff(orient_arr[:,0]).mean()
   p_now = a*time_orient**2 + b*time_orient + c
   w_now = 2*a*time_orient + b
   wdot_now = 2*a
   
-  tl = orient_arr[0,0]
-  w_last = 2*a*tl + b
+  w_last = 2*a*t2 + b
   wdot_last = 2*a
    
   orient_arr[1:,:] = orient_arr[:-1,:]
-  orient_arr[1,2:-1] = w_last, wdot_last
-  orient_arr[0,:] = time_orient, p_now, w_now, wdot_now, p_now
+  orient_arr[1,2:4] = w_last, wdot_last
+  orient_arr[0,:] = orient_arr[0,0] + time_orient, p_now, w_now, wdot_now, p_now, orient_arr[0,0] + time_orient
 
   return orient_arr
 
@@ -65,31 +68,36 @@ def predict_orient(orient_arr, time_orient):
 i = 0
 orient = 0 
 wait_sum = 0
-ni = 1
+ni = 5
 cmd = [0,1,1]
 t_init = time.time()
 t0 = 0
-while True: #:orient not in [0, 1023]:
+t_add = .1
+while True: 
 
   # time pitch is measure is roughly here...  
-  time_orient = time.time() - t_init
   talk(ser, cmd)
-  orient_arr = predict_orient(orient_arr, time_orient)
+  orient_arr = predict_orient(orient_arr, t_add)
 
   cmd = act(orient_arr)
-  orient, wait = listen(ser)
+  orient, imu_time, wait = listen(ser)
 
-  orient_arr = update_orient(orient_arr, orient, time_orient)
-
+  orient_arr = update_orient(orient_arr, orient, imu_time)
 
   wait_sum += wait
 
-  i+=1
   if i%ni == 0:
     t1 = time.time()
-    print('Time per 1 loop: {:.03f}ms'.format((t1-t0)*1000/ni))
-    print('Pitch = {:.2f}deg, Freq: {}'.format(orient, ni/(t1-t0)))
+    print('Freq: ', ni/(t1-t0))
     print('Fraction time waiting serial: {:.3f}'.format(wait_sum/(t1-t0)))
-    print(orient_arr)
+    if report:
+      print(orient_arr)
+      print((orient_arr[:,0] - orient_arr[:, -1]).mean(), t_add)
+      print(orient_arr[:,1] - orient_arr[:, -2])
+
+    print()
+    t_add = np.diff(orient_arr[::-1,0]).mean()
     wait_sum = 0
     t0 = time.time()
+
+  i+=1
