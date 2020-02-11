1af7fb99cfa3bda6bf2caee762d0ca7054c12595
TopiKo <topiko1987@gmail.com>
2020-02-11 19:26:05 +0100

    Report routine - requires ssh keys to be set with robot.

---
+++ Analyze/plot_orient.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import subprocess
+
+subprocess.call(['scp', 'sexybot:orient.npy', '.'])
+orient_arr = np.load('orient.npy')
+
+times = orient_arr[:, 0]
+phi = orient_arr[:, 1]
+phidot = orient_arr[:, 2]
+phidotdot = orient_arr[:, 3]
+
+fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = (12,8), sharex = True)
+
+ax1.plot(times, phi)
+ax1.set_title('phi')
+ax2.plot(times, phidot)
+ax2.set_title('phidot')
+ax3.plot(times, phidotdot)
+ax3.set_title('phidotdot')
+ax3.set_xlabel('time')
+plt.show()
+
