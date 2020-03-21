dc936d4c3d6a2a59133751289a06c4aa61958b2c
TopiKo <topiko1987@gmail.com>
2020-03-21 13:00:45 +0100

    Start major revision

--- Analyze/plot_orient.py
+++ Analyze/plot_orient.py
@@ -1,21 +1,23 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import subprocess
-import datetime
-from scipy.misc import derivative
-from scipy.optimize import curve_fit
+
 import sys
 import os
+import subprocess
+import datetime
 
-if len(sys.argv) == 2: mode = sys.argv[1]
-else: mode = 'read_rpi'
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import derivative
+from scipy.optimize import curve_fit
 
-predict = True
+if len(sys.argv) == 2: MODE = sys.argv[1]
+else: MODE = 'read_rpi'
 
-if mode == 'read_rpi':
-    subprocess.call(['scp', 'sexybot:orient.npy', '.'])
+PREDICT = True
+
+if MODE == 'read_rpi':
+    subprocess.call(['scp', 'sexybot:bBot/orient.npy', '.'])
     orient_arr = np.load('orient.npy')
-elif mode == 'history':
+elif MODE == 'history':
     files = os.listdir('./datas')
     orient_arr = np.load('datas/' + files[2])
 
@@ -31,14 +33,17 @@ phi_pred = orient_arr[ndiscard:, 5]
 phidot_pred = orient_arr[ndiscard:, 6]
 phidotdot_pred = orient_arr[ndiscard:, 7]
 
-def func(t, a,b,c):
-    
+vl = orient_arr[ndiscard:, 8]
+vr = orient_arr[ndiscard:, 9]
+al = orient_arr[ndiscard:, 10]
+ar = orient_arr[ndiscard:, 11]
+
+def func(t, a, b, c):
     return a*t**2 + b*t + c
 
 def get_phidot_phidotdot(i, n):
-    
-    
-    if predict: n_ = 1
+
+    if PREDICT: n_ = 1
     else: n_ = 0
 
     if i < n+n_: return 0, 0
@@ -46,14 +51,12 @@ def get_phidot_phidotdot(i, n):
     t_ = times[i-n-n_:i-n_]
     phi_ = phi[i-n-n_:i-n_]
     (a,b,c), _ = curve_fit(func, t_, phi_, (1,1,1))
-    
     phidot = derivative(func, times[i], dx = .05, args = (a,b,c))
-    phidotdot = derivative(func, times[i], n = 2, dx = .05, args =(a,b,c))   
-    
+    phidotdot = derivative(func, times[i], n = 2, dx = .05, args =(a,b,c))
     return phidot, phidotdot
 
 def get_phidot_phidotdot_arr(times, n):
-    
+
     phidot_arr = np.zeros(len(times))
     phidotdot_arr = np.zeros(len(times))
     for i in range(len(times)):
@@ -63,29 +66,50 @@ def get_phidot_phidotdot_arr(times, n):
 
     return phidot_arr, phidotdot_arr
 
-fig, (ax1, ax2, ax3) = plt.subplots(3, figsize = (12,8), sharex = True)
+fig, (ax1, ax2, ax3, ax4, ax5) = plt.subplots(5, figsize = (12,8), sharex = True)
 
-#phidot_loc, phidotdot_loc = get_phidot_phidotdot_arr(times, 4) 
 s = 5
+# Phi:
+#=======================================================
 ax1.plot(times, phi, '-o', markersize = s, label = 'rpi')
 ax1.plot(times_pred, phi_pred, '-o', markersize = s, label = 'pred_rpi')
 for i in range(len(times)-1):
     ax1.plot([times[i], times_pred[i]], [phi[i], phi_pred[i]], lw = .5, color = 'black')
 ax1.legend()
 ax1.set_title('phi')
+#=======================================================
 
+# phi dot
+#=======================================================
 ax2.plot(times, phidot, '-o', markersize = s, label = 'rpi')
 ax2.plot(times_pred, phidot_pred, '-o', markersize= s, label = 'pred')
+for i in range(len(times)-1):
+    ax2.plot([times[i], times_pred[i]], [phidot[i], phidot_pred[i]], lw = .5, color = 'black')
 ax2.plot(times, np.gradient(phi, times), markersize = s, label = 'grad')
 ax2.set_title('phidot')
 ax2.legend()
+#=======================================================
 
+# Phi dodtot
+#=======================================================
 ax3.plot(times, phidotdot, '-o', markersize = s, label = 'rpi')
 ax3.plot(times_pred, phidotdot_pred, '-o', markersize = s, label = 'pred')
 ax3.plot(times, np.gradient(np.gradient(phi, times), times), markersize = s, label = 'grad')
 ax3.set_title('phidotdot')
 ax3.legend()
-ax3.set_xlabel('time')
+#=======================================================
+
+# Vleft and right
+#=======================================================
+ax4.plot(times, vl, '-o', markersize = s, label = 'vl')
+ax4.plot(times, vr, '-o', markersize = s, label = 'vr')
+
+# Vleft and right
+#=======================================================
+ax5.plot(times, al, '-o', markersize = s, label = 'al')
+ax5.plot(times, ar, '-o', markersize = s, label = 'ar')
+
+ax5.set_xlabel('time')
 plt.show()
 
 str_time = datetime.datetime.now().strftime("%d-%m_%H:%M")
