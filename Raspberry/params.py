32defcf41bcb59778ca5e499a4b2e35e64b9765a
topiko <topiko1987@gmail.com>
2020-04-21 19:32:56 +0100

    updated params

--- Raspberry/params.py
+++ Raspberry/params.py
@@ -31,7 +31,7 @@ OPM_LOOP_TIME = 5
 try:
     CTRL_PARAMS_DICT = np.load('ctrl_params.npy', allow_pickle=True).item()
 except FileNotFoundError:
-    CTRL_PARAMS_DICT = {'kappa_v':2, # D x * kappa = v_target
+    CTRL_PARAMS_DICT = {'kappa_v':5, # D x * kappa = v_target
                         'kappa_v2':1,
                         'kappa_tilt_theta':.2*GRAVITY_ACCEL, #1000, # D v * kappa = theta tilt
                         'kappa_theta':3, # D theta = exp(-kappa * t)
