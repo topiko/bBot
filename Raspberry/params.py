f7957045a250f1ac475e0f3cd62b02beccba9916
topiko <topiko1987@gmail.com>
2020-06-30 19:20:54 +0100

    Patric is succesfull

--- Raspberry/params.py
+++ Raspberry/params.py
@@ -12,15 +12,15 @@ PI = 3.14159267
 
 DT = .016
 
-AMPLITUDE = .05 #12
+AMPLITUDE = .10 #12
 INIT_THETA_DEV = 1
 
 # Method for optimizing the ctrl parameters: ('L-BFGS-B', 'brute')
 OPM_METHOD = 'diff_evo' #'L-BFGS-B' #'diff_evo' #'L-BFGS-B' #'brute'
 
 # maximum targeted accel:
-MAX_V = 0.1
-MAX_A = 0.3
+MAX_V = 0.5
+MAX_A = 1.0
 MAX_A_CTRL = 30
 MAX_V_CTRL = (1024 * ARDUINO_STEP_MULTIP / STEPS_PER_REV) * PI*WHEEL_DIA
 
@@ -39,8 +39,8 @@ ALPHA_SIMUL = ALPHA # - 30
 GRAVITY_ACCEL = 9.81
 
 # Uncertainty in theta measurement
-SIGMA_THETA = .0050**2 # .015**2
-SIGMA_THETADOTDOT = .015 # 0.15 is a good value
+SIGMA_THETA = .0015**2 # .015**2
+SIGMA_THETADOTDOT = .002 # 0.15 is a good value
 KL_USE_INPUT = True #False #True #False
 
 SIMUL_LOOP_TIME = 22.5
@@ -52,10 +52,10 @@ KAPPA_D_THETA = None #1/2
 try:
     CTRL_PARAMS_DICT = np.load('ctrl_params.npy', allow_pickle=True).item()
 except FileNotFoundError:
-    CTRL_PARAMS_DICT = {'P_pos':15,
-                        'D_pos':2,
-                        'I_pos':0.0,
-                        'P_theta':100,
+    CTRL_PARAMS_DICT = {'P_pos':10,
+                        'D_pos':3,
+                        'I_pos':1.0,
+                        'P_theta':150,
                         'D_theta':20,
                         'I_theta':10, #1.5,
                         'accel_mltp':1.0}
